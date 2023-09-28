e119fe0da95741b2a358c7232e75761791bac0ce
Juno Joseph <junojoseph@hotmail>
2023-09-28 11:31:12 +0200

    Made css layout for app

--- src/App.js
+++ src/App.js
@@ -7,11 +7,14 @@ const API_URL = 'http://www.omdbapi.com?apikey=56ba107c';
 const App = () => {
 
     const searchMovies =  async (title) => {
-        const response = await fetch('${API_URL}&s=${title}');
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.search);
     }
 
     useEffect(() =>  {
-
+          searchMovies('Spiderman');
 
     }, []);
 
