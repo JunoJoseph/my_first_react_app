11d222b7c7c3f51f8894e143ebf036b3439aded3
Juno Joseph <junojoseph@hotmail>
2023-09-28 10:54:27 +0200

    make a function to call the api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from "react";
+import { useEffect } from "react";
+//56ba107c
+
+const API_URL = 'http://www.omdbapi.com?apikey=56ba107c';
+
 
 const App = () => {
+
+    const searchMovies =  async (title) => {
+        const response = await fetch('${API_URL}&s=${title}');
+    }
+
+    useEffect(() =>  {
+
+
+    }, []);
+
     return (
         <h1>App</h1>
     );
